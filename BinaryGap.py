9c8538bc01c38eddaabf63cbe5e1a771ab629929
Mr-Mount <48280461+Mr-Mount@users.noreply.github.com>
2020-04-07 14:11:14 +0530

    programs modified

--- BinaryGap.py
+++ BinaryGap.py
@@ -1,5 +1,12 @@
 #BinaryGap
 #Find longest sequence of zeros in binary representation of an integer.
+'''
+given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
+
+For example, given N = 1041 the function should return 5, because N has binary representation 10000010001
+and so its longest binary gap is of length 5.
+Given N = 32 the function should return 0, because N has binary representation '100000' and thus no binary gaps.
+''''
 
 def binaryGap(N):
     N = bin(N).replace("0b", "")
--- cyclic rotation.py
+++ cyclic rotation.py
@@ -1,5 +1,29 @@
 #CyclicRotation
 #Rotate an array to the right by a given number of steps.
+'''
+The goal is to rotate array A K times; that is, each element of A will be shifted to the right K times.
+Write a function:
+def solution(A, K)
+that, given an array A consisting of N integers and an integer K, returns the array A rotated K times.
+For example, given
+    A = [3, 8, 9, 7, 6]    K = 3
+the function should return [9, 7, 6, 3, 8]. Three rotations were made:
+
+    [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
+    [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
+    [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
+For another example, given
+
+    A = [0, 0, 0]
+    K = 1
+the function should return [0, 0, 0]
+
+Given
+
+    A = [1, 2, 3, 4]
+    K = 4
+the function should return [1, 2, 3, 4]
+'''
 
 def solution(A, K):
     if len(A) == 0:
