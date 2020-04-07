16f4be15c1b0d6f95750ad39285cceabf610cd76
Mr-Mount <48280461+Mr-Mount@users.noreply.github.com>
2020-04-07 18:12:08 +0530

    tap equilibrium program is added

---
+++ Tape_Equilibrium.py
@@ -0,0 +1,36 @@
+'''
+A non-empty array A consisting of N integers is given. Array A represents numbers on a tape.
+
+Any integer P, such that 0 < P < N, splits this tape into two non-empty parts: A[0], A[1], ..., A[P − 1] and A[P], A[P + 1], ..., A[N − 1].
+
+The difference between the two parts is the value of: |(A[0] + A[1] + ... + A[P − 1]) − (A[P] + A[P + 1] + ... + A[N − 1])|
+
+In other words, it is the absolute difference between the sum of the first part and the sum of the second part.
+
+For example, consider array A such that:
+
+  A[0] = 3
+  A[1] = 1
+  A[2] = 2
+  A[3] = 4
+  A[4] = 3
+We can split this tape in four places:
+
+P = 1, difference = |3 − 10| = 7
+P = 2, difference = |4 − 9| = 5
+P = 3, difference = |6 − 7| = 1
+P = 4, difference = |10 − 3| = 7
+returns the minimal difference that can be achieved.eg:1
+'''
+def solution(A):
+    
+    dummy = []
+    for i in range(len(A)-1):
+        a = A[0:i+1]
+        b = A[i+1:]
+        result = abs(sum(a)-sum(b))
+        dummy.append(result)
+    return min(dummy)
+
+print(solution([3,1,2,3,4]))
+input()
